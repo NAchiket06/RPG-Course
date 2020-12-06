b9af00d8b1950894e7ef9e9538355329a68b7606
Nachiket Jadhav <nachiketjadhav123@yahoo.co.in>
2020-12-06 16:37:37 +0530

    Added second scene and transtions between 2 scenes

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Transition());
+            }
+
+        }
+
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            //SceneManager.LoadScene(sceneToLoad);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("Scene Loaded");
+            Destroy(gameObject);
+        }
+    }
+
+}
