654677dd7c816b4b77cab0751fa4e772748f9a21
Nachiket Jadhav <nachiketjadhav123@yahoo.co.in>
2020-12-06 18:31:22 +0530

    Linked Portal to one another

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+             A,B,C,D,E
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform SpawnPoint;
+        [SerializeField] DestinationIdentifier destinationIdentifier;
         // Start is called before the first frame update
         void Start()
         {
@@ -32,11 +41,41 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+
+            if(sceneToLoad == -1)
+            {
+                Debug.Log("Invalid sceneToLoad Index");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);
             //SceneManager.LoadScene(sceneToLoad);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.SpawnPoint.position);
+            player.transform.rotation = otherPortal.SpawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destinationIdentifier == this.destinationIdentifier) continue;
+                
+                else return portal;
+
+            }
+
+            return null;
         }
     }
 
